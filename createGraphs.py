b1f6f8336188191772bd33a6baeead7a90c5a472
Drothknar <sueanne@cs.utexas.edu>
2018-11-28 14:31:01 -0600

    Reformat to snake case

--- createGraphs.py
+++ createGraphs.py
@@ -1,2 +1,5 @@
-# 
-def RandomEdges():
+# Each connected word represents an integer of the following layout
+# number_of_nodes
+# index_of_node index_of_ending_node_in_edge index_of_ending_node index_of_ending_node
+# ''
+def random_edges():
