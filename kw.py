7428ab68dbe5d8bed6acc8ee2abf77486d8510fd
dopplegangerK <kelsey.pownell@gmail.com>
2018-12-05 23:03:51 -0600

    tried to parallelize in a new way

--- kw.py
+++ kw.py
@@ -1,69 +1,88 @@
 from pyspark import SparkContext, SparkConf
 import spark
-
-def is_valid_node_coloring(item):
-    node_pair, color_pair = item
-    node, edges = node_pair
-    color, color_set = color_pair
-    for edge in edges:
-        if edge in color_set:
-            return False
-    return True
+import math
+import functools
 
 
-def reformat_coloring_pair(item):
-    node_pair, color_pair = item
-    node, edges = node_pair
-    color, color_set = color_pair
-    return (node, color)
+def choose_node_color(colors, node, edges):
+    for color, nodes in colors:
+        color_set = map(lambda x: x[0], nodes)
+        if not any((k in color_set) for k in edges):
+            return color
 
 
-# color_assignments: RDD with elements (color, node)
-# edges: RDD with elements (node, [neighbor1, neighbor2, ...])
-def reduce_bin(color_assignments, edges, start_color, end_color):
-    split_color = (end_color - start_color)//2
+# in this bin, recolor nodes of the highest_numbered color in the bin
+def recolor(bin_iter, n, num_colors):
+    bin = list(bin_iter)
+    bin.sort(key=lambda x: x[0])
+    if len(bin) <= num_colors:
+        return []
+    colors_to_keep = bin[:num_colors]
+    color_to_recolor = bin[-1]
+    nodes_to_recolor = color_to_recolor[1]
 
-    # find current color_sets of colors we plan to keep
-    # (color, {node1, node2, ...})
-    color_sets = color_assignments.filter(lambda x: x[0] < split_color).groupByKey().map(lambda x: (x[0], set(x[1])))
+    recolored_nodes = []#{}
+    # parallelize this later
+    for node, edges in nodes_to_recolor:
+        color = choose_node_color(colors_to_keep, node, edges)
+        recolored_nodes.append((color, (node, edges)))
+    return recolored_nodes
 
-    for c in range(split_color, end_color):
-        # get an rdd of the form (node, [neighbor1, neighbor2, ...])
-        # for all nodes of color c (old color to eliminate)
-        color_nodes = color_assignments.filter(lambda x: x[0] == c).join(edges).mapValues(lambda x: x[1])
 
-        # get all possible color assignments for these nodes
-        # ((node, [neighbor1, neighbor2, ...]), (color, {node1, node2}))
-        possible_assignments = color_nodes.cartesian(color_sets)
+def keyPartitioner(key, partition_span):
+    return key // partition_span
 
-        # filter for valid color assignments
-        # (node, color)
-        valid_assignments = possible_assignments.filter(is_valid_node_coloring).map(reformat_coloring_pair)
-
-        # pick lowest valid assignment for each node
-        #print(valid_assignments.collect())
-        new_assignments = valid_assignments.groupByKey()
-        new_assignments = new_assignments.mapValues(min)
-        # flip the pairs around (from (node, color) to (color, node))
-        new_assignments = new_assignments.map(lambda x: (x[1], x[0]))
-
-        # join this with existing color assignments rdd
-        # (leave the old assignments in for now, filter them out at the end)
-        color_sets = color_assignments.filter(lambda x : x[0] < split_color).cogroup(new_assignments).flatMapValues(lambda x: x).map(lambda x: (x[0], set(x[1])))
-        color_assignments = color_sets.flatMapValues(lambda x: x)
-
-    # filter the old colors out of color_assignments
-    color_assignments = color_assignments.filter(lambda x: x[0] < split_color or x[0] >= end_color)
-    # return the modified color assignments
-    return color_assignments
 
 import spark
-def try_reduce_bin(sc):
+def kuhn_wattenhoffer(sc):
     nodes, _ = spark.create_initial_rdds(sc, "inputs/8.txt")
-    color_assignments = nodes.map(lambda x: (x[0], x[0]))
-    color_assignments = reduce_bin(color_assignments, nodes, 0, 8)
+    color_assignments = nodes.map(lambda x: (x[0], (x[0], x[1])))
+    num_nodes = nodes.count()
+    num_colors = num_nodes
+    final_num_colors = int(math.log(num_nodes)) + 1
+
+    while num_colors > final_num_colors:
+        if num_colors <= final_num_colors * 2:
+            num_partitions = 1
+        else:
+            num_partitions = math.ceil(num_colors / (final_num_colors * 2))
+        # partition into bins
+        partition_span = num_nodes // num_partitions
+        partition_func = functools.partial(keyPartitioner, partition_span=partition_span)
+        color_assignments = color_assignments.partitionBy(num_partitions, partitionFunc=partition_func)
+        print(color_assignments.glom().collect())
+
+        # for each bin, loop through colors and recolor those nodes
+        for i in range(math.ceil(num_colors / num_partitions) - final_num_colors):
+            print(i)
+            # get color sets
+            color_sets = color_assignments.groupByKey().partitionBy(num_partitions, partitionFunc=partition_func)
+            # recolor nodes of the highest-valued color in each bin
+            map_func = functools.partial(recolor, n=i, num_colors=final_num_colors)
+            recolored_nodes = color_sets.mapPartitions(map_func)
+            print(recolored_nodes.collect())
+            # filter out old colors from color_assignments
+            # and union with newly colored nodes
+            # TODO: fix cause it's broken
+            color_assignments = color_assignments.filter(lambda x: x[0] % partition_span != final_num_colors + i).union(recolored_nodes)
+            print(color_assignments.collect())
+
+        num_colors = nodes.count()
+
     print(color_assignments.collect())
+
+
+def recolor_something():
+    bin = []
+    bin.append((0, [(1,[]),(2,[]),(3,[])]))
+    bin.append((1, [(4,[]),(5,[]),(6,[])]))
+    bin.append((3, [(9,[1,2]),(10,[4,5])]))
+    bin.append((2, [(7,[1,2]),(8,[4,5])]))
+
+    print(recolor(bin, 0, 2))
+    print(recolor(bin, 1, 2))
+
 
 if __name__ == "__main__":
     conf, sc = spark.setup_spark_context()
-    try_reduce_bin(sc)
+    kuhn_wattenhoffer(sc)
