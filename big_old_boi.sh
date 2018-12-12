25bb448060860bba2da14315fd194915f0fc6256
dopplegangerK <kelsey.pownell@gmail.com>
2018-12-11 18:23:41 -0600

    more inputs and spiffy loops in scripts

--- big_old_boi.sh
+++ big_old_boi.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-./create_cluster.sh
-./submit_test.sh gs://babby-bucket/Inputs/128.txt 
-./delete_cluster.sh
+
+for num_workers in 2 4 8 16; do
+    for num_cores in 2 4; do
+        echo "Cluster with $num_workers workers, $num_cores cores"
+        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+        ./create_cluster.sh $num_workers $num_cores
+
+        for input_file in 2048.txt 8192.txt 32768.txt 131072.txt; do
+            ./submit_test.sh gs://babby-bucket/Inputs/$input_file
+        done
+
+        ./delete_cluster.sh
+    done
+done
--- create_cluster.sh
+++ create_cluster.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-gcloud dataproc clusters create babby --region us-central1 --zone us-central1-f  --master-boot-disk-size=10GB --master-boot-disk-type=pd-standard --master-machine-type=n1-standard-1 --worker-boot-disk-type=pd-standard --worker-boot-disk-size=10GB --worker-machine-type=n1-standard-1 --num-workers=2 --initialization-actions gs://babby-bucket/initialization.sh
+NUM_WORKERS=$1
+NUM_CORES=$2
+
+gcloud dataproc clusters create babby --region us-central1 --zone us-central1-f  --master-boot-disk-size=10GB --master-boot-disk-type=pd-standard --master-machine-type=n1-standard-1 --worker-boot-disk-type=pd-standard --worker-boot-disk-size=10GB --worker-machine-type=n1-standard-$NUM_CORES --num-workers=$NUM_WORKERS --initialization-actions gs://babby-bucket/initialization.sh
