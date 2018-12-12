f61bed41ae52ae755a7112dbad3f367165e7898d
dopplegangerK <kelsey.pownell@gmail.com>
2018-12-11 18:24:53 -0600

    submit parallel job

--- big_old_boi.sh
+++ big_old_boi.sh
@@ -9,7 +9,7 @@ for num_workers in 2 4 8 16; do
         ./create_cluster.sh $num_workers $num_cores
 
         for input_file in 2048.txt 8192.txt 32768.txt 131072.txt; do
-            ./submit_test.sh gs://babby-bucket/Inputs/$input_file
+            ./submit_parallel_job.sh gs://babby-bucket/Inputs/$input_file
         done
 
         ./delete_cluster.sh
